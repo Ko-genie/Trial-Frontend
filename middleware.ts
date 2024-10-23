d93960f83c385db6bc61b75e846f6197935f6381
aaviix <singhavanish8642@gmail.com>
2024-10-23 17:49:04 +0530

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,55 +1,22 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import { authMiddleware } from "@clerk/nextjs/server";
-import { RedirectToSignIn } from "@clerk/nextjs";
 
-export default authMiddleware({
-  publicRoutes: ['/'],  // Handle dynamic segments with :param syntax
-  afterAuth(auth, req) {
-    // Ensure that 'auth' is properly initialized
-    if (!auth) {
-      return NextResponse.error(new Error("Authentication object is missing"));
+export default clerkMiddleware((req, { auth }) => {
+  const { userId, orgId } = auth;
+
+  if (userId) {
+    let path = "/select-org";
+    if (orgId) {
+      path = `/organization/${orgId}`;
     }
 
-    // If the user is authenticated and the route is public, redirect to organization selection or organization page
-    if (auth.userId && auth.isPublicRoute) {
-      let path = "/select-org";
-
-      // Redirect to the organization page if the user has an organization
-      if (auth.orgId) {
-        path = `/organization/${auth.orgId}`;
-      }
-
-      const orgSelection = new URL(path, req.url);
-      return NextResponse.redirect(orgSelection);
-    }
-
-    // If the user is not authenticated and the route is not public, redirect to sign-in
-    // if (!auth.userId && !auth.isPublicRoute) {
-    //   return RedirectToSignIn({ redirectUrl: req.url });
-    // }
-
-    // If the user is authenticated but hasn't selected an organization, redirect to organization selection
-    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
-      const orgSelection = new URL("/select-org", req.url);
-      return NextResponse.redirect(orgSelection);
-    }
-
-    // Allow the request to continue if no redirect conditions are met
-    return NextResponse.next();
+    const orgSelection = new URL(path, req.url);
+    return NextResponse.redirect(orgSelection);
   }
+
+  return NextResponse.next();
 });
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
-
-// import { NextResponse } from "next/server";
-// import { authMiddleware } from "@clerk/nextjs/server";
-
-// export default authMiddleware({
-//   publicRoutes: ['/', '/organization/:organizationId/createAd'],  // Ensure this matches your dynamic route
-// });
-
-// export const config = {
-//   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
